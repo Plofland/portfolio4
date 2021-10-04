5f67d99a930c0d2dd7d7215808ae98c7796d9c74
Plofland <ploflan@gmail.com>
2021-10-04 16:05:36 -0400

    Sass styling set up

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import projectsList from '../projectsList'
+import ProjectCard from './ProjectCard';
 
 export default function Projects() {
-    return (
-        <div>
-            
-        </div>
-    )
+
+	return (
+		<ProjectsSection className='projectSection' id="projects">
+			<SectionTitle>Projects</SectionTitle>
+			<ProjectsContainer>
+				{projectsList.map((project) => {
+					return (
+						<ProjectCard
+							key={project.id}
+							project={project}
+						/>
+					);
+				})}
+			</ProjectsContainer>
+		</ProjectsSection>
+	);
 }
+
+const ProjectsSection = styled.div`
+	display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+	padding: 2rem;
+	background-color: blue;
+`;
+
+const SectionTitle = styled.div`
+	font-size: 3rem;
+`;
+
+const ProjectsContainer = styled.div`
+	display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+`;
---
+++ src/styles/themes.js
@@ -0,0 +1,10 @@
+export const colors = {
+	forrestGreen: '#3A5953',
+	darkBlue: '#2A2A3B',
+	ivory: '#E0DDE1',
+	midnightBlue: '#3A5673',
+	turquoise: '#51B9CD',
+	darkText: '#222222',
+	lightText: '#E7FEFE'
+};
+
