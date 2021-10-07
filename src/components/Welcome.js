0245c910a581e6612016da5507ae45dc5c77af51
Plofland <ploflan@gmail.com>
2021-10-07 16:13:06 -0400

    Welcome hero header better styled

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import '../styles/Welcome.scss'
+import '../styles/Welcome.scss';
 
 export default function Welcome() {
 	return (
 		<div className="welcome">
-			<h2>Hello There</h2>
+			<div id="headerHolder">
+				<h2>Hello There</h2>
+			</div>
 		</div>
 	);
 }
