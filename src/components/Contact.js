6059d9cba42dd77b78e270995ed01bbc3bde28c3
Plofland <ploflan@gmail.com>
2021-10-10 17:35:47 -0400

    Contact section transferred over to styled-components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import '../styles/Contact.scss';
 import Fade from 'react-reveal/Fade';
+import styled from 'styled-components';
 import { useMediaQuery } from 'react-responsive';
+import { colors } from '../styles/themes';
+
+//Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import {
@@ -12,14 +15,14 @@ import {
 } from '@fortawesome/free-brands-svg-icons';
 
 function Contact() {
-    const isMobile = useMediaQuery({
+	const isMobile = useMediaQuery({
 		query: '(max-width: 750px)'
 	});
 	return (
-		<div className="socialIcons">
+		<ContactSection>
 			<Fade bottom cascade delay={400}>
-				<ul>
-					<li>
+				<SocialIcons>
+					<Icon>
 						<a
 							href={
 								'https://mail.google.com/mail/?view=cm&fs=1&to=ploflan@gmail.com&su=SUBJECT&body=BODY'
@@ -28,21 +31,25 @@ function Contact() {
 							rel="noreferrer"
 							aria-label="Gmail"
 						>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal>
 								<FontAwesomeIcon
 									icon={faEnvelope}
 									className="fa fa-envelope"
-                                    size={isMobile ? 'xs' : null}
+									size={
+										isMobile
+											? 'xs'
+											: null
+									}
 								/>
-							</span>
+							</IconPedal>
 						</a>
-					</li>
-					<li>
+					</Icon>
+					<Icon>
 						<a
 							href={
 								'https://www.linkedin.com/in/peter-lofland/'
@@ -51,21 +58,25 @@ function Contact() {
 							rel="noreferrer"
 							aria-label="LinkedIn"
 						>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal>
 								<FontAwesomeIcon
 									icon={faLinkedin}
 									className="fa fa-linkedin"
-                                    size={isMobile ? 'xs' : null}
+									size={
+										isMobile
+											? 'xs'
+											: null
+									}
 								/>
-							</span>
+							</IconPedal>
 						</a>
-					</li>
-					<li>
+					</Icon>
+					<Icon>
 						<a
 							href={
 								'https://twitter.com/PeterLofland'
@@ -73,23 +84,26 @@ function Contact() {
 							target="_blank"
 							rel="noreferrer"
 							aria-label="Twitter"
-                            
 						>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal>
 								<FontAwesomeIcon
 									icon={faTwitter}
 									className="fa fa-twitter"
-                                    size={isMobile ? 'xs' : null}
+									size={
+										isMobile
+											? 'xs'
+											: null
+									}
 								/>
-							</span>
+							</IconPedal>
 						</a>
-					</li>
-					<li>
+					</Icon>
+					<Icon>
 						<a
 							href={
 								'https://github.com/Plofland'
@@ -98,21 +112,25 @@ function Contact() {
 							rel="noreferrer"
 							aria-label="Github"
 						>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal>
 								<FontAwesomeIcon
 									icon={faGithub}
 									className="fa fa-github"
-                                    size={isMobile ? 'xs' : null}
+									size={
+										isMobile
+											? 'xs'
+											: null
+									}
 								/>
-							</span>
+							</IconPedal>
 						</a>
-					</li>
-					<li>
+					</Icon>
+					<Icon>
 						<a
 							href={
 								'https://www.instagram.com/peterlofland/'
@@ -121,24 +139,118 @@ function Contact() {
 							rel="noreferrer"
 							aria-label="Instagram"
 						>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							<span>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal></IconPedal>
+							<IconPedal>
 								<FontAwesomeIcon
 									icon={faInstagram}
 									className="fa fa-instagram"
-                                    size={isMobile ? 'xs' : null}
+									size={
+										isMobile
+											? 'xs'
+											: null
+									}
 								/>
-							</span>
+							</IconPedal>
 						</a>
-					</li>
-				</ul>
+					</Icon>
+				</SocialIcons>
 			</Fade>
-		</div>
+		</ContactSection>
 	);
 }
 
 export default Contact;
+
+const ContactSection = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+`;
+
+const SocialIcons = styled.ul`
+	position: relative;
+	display: flex;
+	justify-content: space-evenly;
+	padding: 0;
+
+	li:nth-child(1) span {
+		background: #de5246;
+	}
+	li:nth-child(2) span {
+		background: #0072b1;
+	}
+	li:nth-child(3) span {
+		background: #00acee;
+	}
+	li:nth-child(4) span {
+		background: #171515;
+	}
+	li:nth-child(5) span {
+		background: #3f729b;
+	}
+`;
+const Icon = styled.li`
+	position: relative;
+	list-style: none;
+	width: 4rem;
+	height: 4rem;
+	margin: 2rem 2rem;
+
+	&:hover {
+		box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.2);
+	}
+
+	&:hover span:nth-child(6){
+		transform: scale(1.2);
+			opacity: 1;
+	}
+	&:hover span:nth-child(5){
+		transition-delay: 0.2s;
+			transform: scale(1.2) rotate(45deg);
+			background-color: ${colors.ivory};
+			border-radius: 1px;
+			opacity: 1;
+	}
+	&:hover span:nth-child(4){
+		transform: translate(20px, -20px);
+			transition-delay: 0.05s;
+			border-radius: 40px;
+			opacity: 0.6;
+	}
+	&:hover span:nth-child(3){
+		transform: translate(-20px, 20px);
+			border-radius: 40px;
+			transition-delay: 0.05s;
+			opacity: 0.6;
+	}
+	&:hover span:nth-child(2){
+		transform: translate(-20px, -20px);
+			border-radius: 40px;
+			transition-delay: 0.05s;
+			opacity: 0.6;
+	}
+	&:hover span:nth-child(1){
+		transform: translate(20px, 20px);
+			border-radius: 40px;
+			transition-delay: 0.05s;
+			opacity: 0.6;
+	}
+`;
+const IconPedal = styled.span`
+	border-radius: 10px;
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	transition: 0.5s;
+	display: flex !important;
+	align-items: center;
+	justify-content: center;
+	color: ${colors.ivory};
+	font-size: 3rem;
+`;
