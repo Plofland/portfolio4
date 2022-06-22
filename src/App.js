3b90f49218c4645c4935a44dd321fc09d18b48b6
Plofland <ploflan@gmail.com>
2022-06-22 17:39:33 -0400

    reset App back to default, remove needless dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import 'fullpage.js/vendors/scrolloverflow'; // Optional. When using scrollOverflow:true
-import ReactFullpage from '@fullpage/react-fullpage';
 
 //Components
 import Projects from './components/Projects';
@@ -10,42 +8,16 @@ import Welcome from './components/Welcome';
 import About from './components/About';
 
 import './styles/App.scss';
-import { colors } from './styles/themes';
+// import { colors } from './styles/themes';
 
-class MySection extends React.Component {
-	render() {
-		return (
-			<div className="section">
-				{this.props.content}
-			</div>
-		);
-	}
-}
-
-const anchors = ['Welcome', 'About', 'Skills', 'Projects'];
-
-const App = () => (
-	<ReactFullpage
-		anchors={anchors}
-		navigation
-		sectionsColor={[
-			`${colors.cyanBlue}`,
-			`${colors.oxfordBlue}`,
-			`${colors.cyanBlue}`,
-			`${colors.mineralGreen}`
-		]}
-		render={({ state, fullpageApi }) => {
-			console.clear()
-			return (
-				<div>
-					<MySection content={<Welcome />} />
-					<MySection content={<About />} />
-					<MySection content={<Skills />} />
-					<MySection content={<Projects />} />
-				</div>
-			);
-		}}
-	/>
-);
-
+const App = () => {
+	return (
+		<div>
+			<Welcome />
+			<About />
+			<Skills />
+			<Projects />
+		</div>
+	);
+};
 export default App;
